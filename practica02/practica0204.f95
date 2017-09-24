a1512f621ba90623caf4157387e60f67d0b6eb37
viperML <ayatsfer@gmail.com>
2017-09-24 18:16:41 +0200

    04 terminado

--- practica02/practica0204.f95
+++ practica02/practica0204.f95
@@ -1,29 +1,41 @@
-program funcion
+program funcion_valores
 implicit none
 
-real :: x, y
+real :: x
+real :: y
 real :: resultado
 
-write(*,*) 'Introduce valores de x y'
-read(*,*) x, y
+write(*,*) 'Introduce los valores de x e y:'
+read(*,*) x,y
 
-if (x<y) then
+if (x < y) then
 
-    if (x<-2.) then
+    if (x < -2.) then
         resultado = 0.
-    else if (x<2. .and x>=-2.) then
-        resultado = ((x**2.) + (y**2.) )**(1./2.)
-    else if (x<=4. .and. x>=2.) then
-        resultado = x/(2.*y)
+    else if (-2. <= x .and. x < 2.) then
+        resultado = (x**2 +y**2)**1./2.
+    else if (2. <= x .and. x <= 4.) then
+        resultado = x / (y*2.)
     else
-        resultado = 7.*(x)**(4./3.)
+        resultado = 7*x**(4./3.)
     end if
 
-else if (x > y .and. y > -7)
-    write(*,*) 'relleno'
+else if(x > y .and. y > -7.) then
+
+    if (x < -2.) then
+        resultado = y - x
+    else if (y /= 0) then
+        resultado = x / y
+    else
+        resultado = 0.
+    end if
+
 else
-     write(*,*) 'relleno'
+
+    write(*,*) 'else'
+    resultado = 7*x**3. + 2*x**2. - x + 5
+
 end if
 
-
-end program funcion
+write(*,*) 'f(x,y) =', resultado
+end program funcion_valores
