9e7cc56d34c27a7c3c9faf92608e903945a720bf
viperML <ayatsfer@gmail.com>
2017-09-25 20:32:41 +0200

    Eliminadas condiciones concurrentes

--- practica02/practica0204.f95
+++ practica02/practica0204.f95
@@ -11,10 +11,10 @@ if (x < y) then
 
     if (x < -2.) then
         resultado = 0.
-    else if (-2. <= x .and. x < 2.) then
-        resultado = (x**2 +y**2)**1./2.
-    else if (2. <= x .and. x <= 4.) then
-        resultado = x / (y*2.)
+    else if (x < 2.) then                   !! No se tiene en cuenta el intervalo anterior porque aunque en notacion matematica tiene sentido, la ejecución es secuencial
+        resultado = (x**2 +y**2)**1./2.     !! y los valores que pasen a la siguiente condición ya cumpliran la anterior de una manera u otra.
+    else if (x <= 4.) then                  !! De esta forma evito usar .AND. para ahorrar espacio, ya que no hace ninguna funcion
+        resultado = x / (y*2.)              !! Aun asi, en practica0204.dat estan especificados los intervalos completos
     else
         resultado = 7*x**(4./3.)
     end if
@@ -23,18 +23,19 @@ else if(x > y .and. y > -7.) then
 
     if (x < -2.) then
         resultado = y - x
-    else if ( -2. <= x .AND. x < 2. .AND. y /= 0.) then
+    else if (x < 2. .AND. y /= 0.) then
         resultado = x / y
-    else if ( -2. <= x .AND. x < 2. .AND. y == 0.) then
+    else if (x < 2. .AND. y == 0.) then
         resultado = 0.
     end if
 
 else
 
-    write(*,*) 'else'
     resultado = 7*x**3. + 2*x**2. - x + 5
 
 end if
 
 write(*,*) 'f(x,y) =', resultado
+
+
 end program funcion_valores
